81d2d50d267e989b4004cc099f8227fe8fb1ec6a
Holkan <luah613@gmail.com>
2017-02-15 21:28:22 -0600

    analytics

--- themes/charitywp/header.php
+++ themes/charitywp/header.php
@@ -22,6 +22,17 @@
     <script src="http://www.ojosquesienten.com/js/init.js"></script>
     <!-- Engage Web Tracking Code -->
 
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-91771961-3', 'auto');
+      ga('send', 'pageview');
+
+    </script>
+
 
 <!-- Silverpoop Tracking -->
 <meta name="com.silverpop.brandeddomains" content="www.pages05.net,dev.goigni.com,goigni.com,goigni.net,ignisa.mkt4247.com,ojosquesienten.com,pages05.net" />
