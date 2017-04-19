9daf63225dbde70e8f9249ef217a17a6951cb061
Holkan <luah613@gmail.com>
2017-04-18 21:40:31 -0500

    gallery jetpack

--- themes/charitywp-child/single-galerias.php
+++ themes/charitywp-child/single-galerias.php
@@ -6,26 +6,14 @@
 		<?php	
 			while(have_posts()):
 				the_post();
-
-				$content = get_the_content();
-				// $content = explode($content);
-				$content = preg_replace("/[^0-9,.]/", "", $content);
-				$content = explode(',', $content);
 			?>
 				<div class="col-md-12">
 					<?php the_title(); ?>
 				</div>
-
-		<?php		
-				foreach ($content as $img_id) {
-					?>
-					<div class="col-md-4 img_container">
-						<?php echo wp_get_attachment_image($img_id, 'large')."<br>"; ?>
-					</div>
-
+				<div class="col-md-12 img_container">
+					<?php the_content(); ?>
+				</div>
 					<?php
-				}
-
 			endwhile;
 			?>
 		</section>
