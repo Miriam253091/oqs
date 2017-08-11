70246390ab3c71be074036c4469833941a61e9b3
Holkan <luah613@gmail.com>
2017-08-11 08:04:10 -0500

    patrocinadores

---
+++ themes/charitywp-child/page-concierto-en-la-oscuridad.php
@@ -0,0 +1,76 @@
+
+<?php 
+/*
+	template: cena
+*/
+get_header(); ?>
+	<section id="land_dark_dinner">
+		<header class="article_header">
+			<p class="subtitle">
+				SAVE THE DATE
+			</p>
+		</header>
+		<article class="the_article textwidget">
+			<p class="_parr">Por primera vez en México, te invitamos a vivir un viaje de la oscuridad a la luz</p>
+			<h1 class="event_title">
+				CONCIERTO<br>EN LA OSCURUDAD
+			</h1>
+			<div></div>
+			<time class="the_date">10.10.17</time>
+			<p class="_parr">
+				Lunario del Auditorio Nacional<br>CDMX
+			</p>
+			<p class="_parr oqs_data">
+				Desde2006, Ojos que Sienten ha ofrecido experiencias innovadoras y de impacto social,logrando transformar vidas de personas con y sin discapacidad visual.
+			</p>
+			<p class="_parr mini_txt">
+				Evento de recaudación de fondos
+			</p>
+			<div class="sponsor_pool">
+				<ul class="sponsor_list ul_1" >
+					<li class="sponsor_item">
+						<img src="http://localhost:8888/OQS/wp-content/uploads/2017/07/HSBC.jpg">
+					</li>
+					<li class="sponsor_item">
+						<img src="http://www.ojosquesienten.com/wp-content/uploads/2017/08/02_chubs.png">
+					</li>
+				</ul>
+				<ul class="sponsor_list ul_2" >	
+					<li class="sponsor_item">
+						<img src="http://www.ojosquesienten.com/wp-content/uploads/2017/08/03_qualcom.png">
+					</li>
+					<li class="sponsor_item">
+						<img src="http://www.ojosquesienten.com/wp-content/uploads/2017/08/04_monex.png">
+					</li>
+				</ul>
+				<ul class="sponsor_list ul_3" >
+					<li class="sponsor_item">
+						<img src="http://www.ojosquesienten.com/wp-content/uploads/2017/08/05_cinepolis.png">
+					</li>
+					<li class="sponsor_item">
+						<img src="http://www.ojosquesienten.com/wp-content/uploads/2016/08/Intercontinental-150.jpg">
+					</li>
+					<li class="sponsor_item">
+						<img src="http://www.ojosquesienten.com/wp-content/uploads/2017/08/07_manpower.png">
+					</li>
+				</ul>
+				<ul class="sponsor_list ul_4" >
+					<li class="sponsor_item" >
+						<img src="http://www.ojosquesienten.com/wp-content/uploads/2017/08/08_cannon.png">
+					</li>
+					<li class="sponsor_item" >
+						<img src="http://www.ojosquesienten.com/wp-content/uploads/2017/07/charityPulse-logo-4Creators01.png">
+					</li>
+					<li class="sponsor_item" >
+						<img src="http://www.ojosquesienten.com/wp-content/uploads/2017/07/lunario.jpg">
+					</li>
+					<li class="sponsor_item" >
+						<img src="http://www.ojosquesienten.com/wp-content/uploads/2017/07/gex_logo_imagen.jpg">
+					</li>
+					<li class="sponsor_item" >
+						<img src="http://www.ojosquesienten.com/wp-content/uploads/2017/08/12_sda.png">
+					</li>
+				</ul>
+			</div>
+		</article>
+	</section>
