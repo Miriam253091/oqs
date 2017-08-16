b82d1c312814b6e50b773412cd48003b2b190f3a
Holkan <luah613@gmail.com>
2017-08-16 09:33:30 -0500

    footer done

--- themes/charitywp-child/footer-landing.php
+++ themes/charitywp-child/footer-landing.php
@@ -1,18 +1,18 @@
 	</body>
-	<footer>
+	<footer id="landing_footer">
 		<section id="the_map_concert">
 			<div>
 				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3762.8412122632735!2d-99.15805138458327!3d19.41926554613781!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x61a70980ea06f162!2sOjos+Que+Sienten!5e0!3m2!1sen!2smx!4v1502663232963" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 			</div>
 			<div>
-				<ul>
-					<li>
+				<ul id="footer_contact_list">
+					<li class="fc_item">
 						<a href="">©2017www.ojosquesienten.com</a>
 					</li>
-					<li>
+					<li class="fc_item">
 						<a href="">(55)5207 04119 / (55)5533 1850</a>
 					</li>
-					<li>
+					<li class="fc_item">
 						<a href="mailto:equipo@ojosquesienten.com">equipo@ojosquesienten.com</a>
 					</li>
 				</ul>
