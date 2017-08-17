f2e2bbe7a0c67c18ec295044393d53e1e9ce0194
Holkan <luah613@gmail.com>
2017-08-16 22:26:04 -0500

    artists page

---
+++ themes/charitywp/inc/templates/concierto-artistas.php
@@ -0,0 +1,67 @@
+<section id="artistas_page">
+		<div class="normal-div">
+			<h2>Conoce a los artistas</h2>
+			<div class="the_lilborder"></div>
+			<ul class="ar_list">
+				<li class="ar_item">
+					<article class="ar_article">
+						<div class="ar_sides ar_img">
+							<img src="http://placehold.it/547x590">
+						</div>
+						<div class="ar_sides ar_bio">
+							<h2>Fausto Palma</h2>
+							<p>Fausto Palma es un compositor y multi-instrumentalista mexicano, nacido en 1976.
+								Su vida está marcada por la movilidad. El periplo inició en 1993, cuando tomó sus primeras lecciones en la guitarra.
+							</p>
+							<p>	
+								Desde entonces, ya sea en La Habana, El Cairo, Nueva Delhi, el Distrito Federal, Londres o Ámsterdam, se ha encontrado con el sarangi, el oud y el cabaquinho, instrumentos a los cuales ha tratado de robar sus misterios.
+							</p>
+							<p>	
+								En 1999, crea Asallam, un grupo con influencias pastorales y sinfónicas, sonidos místicos y misteriosos de Oriente. Su nomadismo lo llevó a Europa, Siria, Egipto, Jordania y Líbano. De su paso por Nueva Delhi, además de su contacto con el sarangi, queda Ghazali (Cero Records, 2004). De regreso en México, el guitarrista se dio a la creación de Petra, grupo que toma su nombre de la ciudad jordana.
+							</p>
+							<p>	
+								El más reciente capítulo de esta historia en construcción es Dawn (Intolerancia, 2009), álbum doble en el que a las influencias ya citadas hay que añadir sicodelia, krautrock, rock progresivo, ritmos antillanos y africanos.
+							</p>
+							<p>	
+								Es el guitarrista de Songs for Eleonor de Andrés Almeida
+							</p>
+							<p>	
+								Entre los años 2010 y 2017 a grabado los discos: Lágrima, Módulos, Primavera Defeña, Strictly Personal.
+							</p>
+							<p>
+								En este momento se encuentra lidereando a Petra en una nueva direccion mas rockera, con gitano turco, sicodelia, y grabando el disco doble Medusa
+							</p>
+						</div>
+					</article>
+				</li>
+				
+				<li class="ar_item">
+					<article class="ar_article">
+						<div class="ar_sides ar_img">
+							<img src="http://placehold.it/547x590">
+						</div>
+						<div class="ar_sides ar_bio">
+							<h2>
+								Maricarmen Graue
+							</h2>
+							<p></p>
+						</div>
+					</article>
+				</li>
+
+				<li class="ar_item">
+					<article class="ar_article">
+						<div class="ar_sides ar_img">
+							<img src="http://placehold.it/547x590">
+						</div>
+						<div class="ar_sides ar_bio">
+							<h2>
+								Artist With-no-name
+							</h2>
+							<p></p>
+						</div>
+					</article>
+				</li>								
+			</ul>
+		</div>
+	</section>
