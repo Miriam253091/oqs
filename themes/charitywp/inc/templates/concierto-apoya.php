c184725ff1f2145c9b868d40cf6549f3319a128f
Holkan <luah613@gmail.com>
2017-08-16 23:20:09 -0500

    apoya

---
+++ themes/charitywp/inc/templates/concierto-apoya.php
@@ -0,0 +1,67 @@
+<section id="apoya_page">
+	<div class="normal-div">
+		<h2>¿Por qué apoyar?</h2>
+		<div class="the_lilborder"></div>
+
+		<div>
+			<div class="ap_sides">
+				<p>
+					Al apoyar esta nueva experiencia sensorial, ayudarás a que más personas con <em>dis</em>Capacidad visual puedan segir desarrollándose enfocados en la habilidad de la <em>dis<em>Capacidad para así lograr ser intrgrados social y laboralmente pra alvcanzar su potencial.
+				</p>
+				<p>
+					Buscamos aumentar el número de talleres de capacitación y generar empleo de freelance a través de nuestras expriencias sensoriales, venta de fotografías y talleres empresariales.
+				</p>
+				<p>
+					Así juntos logramos erradicar la ceguera mental y transformar perspectivas ante la sociedad en general, sobre personas con <em>dis</em>Capacidad.
+				</p>
+			</div>
+			<div class="ap_sides">
+				<h3>Buscamos aumentar el número de talleres de capacitación como:</h3>
+				<ul class="side_ul">
+					<li>
+						<i class="fa fa-globe" aria-hidden="true"></i><p>Taller de ingés básico</p>
+					</li>
+					<li>
+						<i class="fa fa-globe" aria-hidden="true"></i><p>Taller de ingés básico</p>
+					</li>
+					<li>
+						<i class="fa fa-globe" aria-hidden="true"></i><p>Taller de ingés básico</p>
+					</li>
+					<li>
+						<i class="fa fa-globe" aria-hidden="true"></i><p>Taller de ingés básico</p>
+					</li>
+				</ul>
+
+				<ul class="side_ul">
+					<li>
+						<i class="fa fa-globe" aria-hidden="true"></i><p>Taller de ingés básico</p>
+					</li>
+					<li>
+						<i class="fa fa-globe" aria-hidden="true"></i><p>Taller de ingés básico</p>
+					</li>
+					<li>
+						<i class="fa fa-globe" aria-hidden="true"></i><p>Taller de ingés básico</p>
+					</li>
+					<li>
+						<i class="fa fa-globe" aria-hidden="true"></i><p>Taller de ingés básico</p>
+					</li>
+				</ul>
+			</div>
+			<div>
+				<p>
+					Te invitamos a ser una empresa con propósito, que practique la responsabilidad social como una decisión de negocio, con valor agregado y no como caridad. Hoy más que nunca debemos trabajar juntos por un mundo mejor, sumando y complementando nuestro tranbajo, bajo una relación de ganar ganar para transformar millones de vidas.
+				</p>
+				<h3>
+					Gina Badenoch
+					<span>Fundadora</span>
+				</h3>
+			</div>
+		</div>
+	</div>
+
+	<div class="normal-div">
+		<h2>¿Cómo apoyar?</h2>
+		<div class="the_lilborder"></div>
+
+	</div>	
+</section>
